fc7f0edecfba944d478eb9910c9c49dbab69732b
nikramakrishnan <ramakrishnan.nikhil@gmail.com>
2021-10-05 14:55:37 +0000

    Add two numbers.

--- hello.py
+++ hello.py
@@ -3,3 +3,12 @@ print("Line 2")
 
 print("This is my line 1")
 print("I'm the other person")
+
+def add(num1, num2):
+    total = num1+num2
+    return total
+
+num1 = 2
+num2 = 10
+total = add(num1, num2)
+print("Sum of two numbers is", total)
